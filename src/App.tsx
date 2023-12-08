1cf51a90006c2d0fac2fb1f313ff69652fddaacb
Adarsh Rai <adarsh.rai@MAB4889.local>
2023-12-08 17:59:19 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <>
+      Equilibrium #3429
+
+      Our Equilibrium collection promotes balance and calm.
+
+      0.041 ETH
+      3 days left
+
+      Creation of Jules Wyvern
+    </>
+  )
+}
+
+export default App
